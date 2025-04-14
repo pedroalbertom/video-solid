cfe3e40e7c34848e8c37f1d5ca9d2a88d13f7675
Pedro Alberto <pedroalberto@unifor.br>
2025-04-14 14:27:57 -0300

    First commit

---
+++ src/services/product/product.service.implementation.ts
@@ -0,0 +1,65 @@
+import { IProductRepository } from "../../repositories/product/product.repository";
+import { BuyOutputDto, IProductService, ListOutputDto, SellOutputDto } from "./product.service";
+
+export class ProductService implements IProductService{
+
+    private constructor(readonly repository: IProductRepository){}
+
+    public static build(repository: IProductRepository){
+        return new ProductService(repository)
+    }
+
+    public async sell(id: string, amount: number): Promise<SellOutputDto> {
+        const aProduct = await this.repository.find(id)
+        
+        if(!aProduct) throw new Error(`O produto ${id} não foi encontrado`)
+
+        aProduct.sell(amount)
+
+        await this.repository.update(aProduct)
+
+        const output: SellOutputDto = {
+            id: aProduct.id,
+            balance: aProduct.quantity
+        }
+
+        return output
+    }
+
+    public async buy(id: string, amount: number): Promise<BuyOutputDto> {
+        const aProduct = await this.repository.find(id)
+        
+        if(!aProduct) throw new Error(`O produto ${id} não foi encontrado`)
+
+        aProduct.buy(amount)
+
+        await this.repository.update(aProduct)
+
+        const output: BuyOutputDto = {
+            id: aProduct.id,
+            balance: aProduct.quantity
+        }
+
+        return output
+    }
+
+    public async list(): Promise<ListOutputDto> {
+        const aProducts = await this.repository.list()
+
+        const products = aProducts.map(p => {
+            return {
+                id: p.id,
+                name: p.name,
+                price: p.price,
+                balance: p.quantity
+            }
+        })
+
+        const output: ListOutputDto = {
+            products
+        }
+
+        return output
+    }
+
+}
