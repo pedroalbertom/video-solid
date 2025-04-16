0179da515c6aecd44b739e0bec8aa63549421e85
Pedro Alberto <pedroalberto@unifor.br>
2025-04-16 15:38:07 -0300

    fix: fix a typo

--- src/api/express/routes.ts
+++ src/api/express/routes.ts
@@ -5,15 +5,15 @@ import { UserController } from "./controllers/user.controller";
 export async function registerRoutes(api: ApiExpress) {
     const productController = ProductController.build()
 
-    await api.addPostRoute("/products/create", productController.create)
+    await api.addPostRoute("/products", productController.create)
     await api.addPostRoute("/products/:id/buy", productController.buy)
     await api.addPostRoute("/products/:id/sell", productController.sell)
     await api.addGetRoute("/products", productController.list)
 
     const userController = UserController.build()
 
-    await api.addPostRoute("/products/create", userController.create)
-    await api.addGetRoute("/products", userController.list)
-    await api.addPutRoute("/products/:id", userController.update)
-    await api.addDeleteRoute("/products/:id", userController.delete)
+    await api.addPostRoute("/users", userController.create)
+    await api.addGetRoute("/users", userController.list)
+    await api.addPutRoute("/users/:id", userController.update)
+    await api.addDeleteRoute("/users/:id", userController.delete)
 }
