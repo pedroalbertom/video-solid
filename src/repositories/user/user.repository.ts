936fdd1daf01f2eac0bea866f88072a8cba82471
Pedro Alberto <pedroalberto@unifor.br>
2025-04-16 13:09:14 -0300

    feat: create user repository interface

---
+++ src/repositories/user/user.repository.ts
@@ -0,0 +1,8 @@
+import { User } from "../../entities/user";
+
+export interface IUserRepository {
+    save(user: User): Promise<void>,
+    list(): Promise<User[]>,
+    update(user: User): Promise<void>,
+    find(id: string): Promise<User | null>
+}
