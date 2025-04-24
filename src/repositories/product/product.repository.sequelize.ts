7075430312f440301c583af3dbfc1c9814d50d07
Pedro Alberto <pedroalberto@unifor.br>
2025-04-24 14:37:59 -0300

    add product sequelize repository

---
+++ src/repositories/product/product.repository.sequelize.ts
@@ -0,0 +1,44 @@
+import { Product } from "../../entities/product/product";
+import { IProductRepository } from "./product.repository";
+import { ProductModel } from "../../models/product.model";
+
+export class ProductRepositorySequelize implements IProductRepository {
+    private constructor() { }
+
+    public static build() {
+        return new ProductRepositorySequelize();
+    }
+
+    public async save(product: Product): Promise<void> {
+        await ProductModel.create({
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            quantity: product.quantity
+        });
+    }
+
+    public async list(): Promise<Product[]> {
+        const models = await ProductModel.findAll();
+
+        return models.map(p => Product.with(p.id, p.name, p.price, p.quantity));
+    }
+
+    public async update(product: Product): Promise<void> {
+        await ProductModel.update({
+            name: product.name,
+            price: product.price,
+            quantity: product.quantity
+        }, {
+            where: { id: product.id }
+        });
+    }
+
+    public async find(id: string): Promise<Product | null> {
+        const model = await ProductModel.findByPk(id);
+
+        if (!model) return null;
+
+        return Product.with(model.id, model.name, model.price, model.quantity);
+    }
+}
