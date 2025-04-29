82567802ddc60666347fb298ed678c285c461395
Pedro Alberto <pedroalberto@unifor.br>
2025-04-29 14:34:08 -0300

    add auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { sendResponse } from "../util/http.functions";
+
+const secret = process.env.JWT_SECRET || "secreta";
+
+export async function authMiddleware(
+    request: Request | FastifyRequest,
+    response: Response | FastifyReply,
+    next?: () => void
+) {
+    const authHeader = request.headers["authorization"];
+    const token = authHeader?.split(" ")[1];
+
+    if (!token) {
+        return sendResponse(response, 401, { error: "Token ausente" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, secret);
+        (request as any).user = decoded;
+
+        if (next) return next(); // Express
+    } catch (err) {
+        return sendResponse(response, 401, { error: "Token inválido" });
+    }
+}
